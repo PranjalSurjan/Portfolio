c2f908dd3ec336f9f57ada11b7760c0bb784c7c8
Pranjal <pranjalsurjan03@gmail.com>
2026-05-20 23:16:05 -0400

    add routing and vercel config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Experience from './pages/Experience'
+import Education from './pages/Education'
+
 function App() {
   return (
     <div>
-      <h1>Hey, I'm Pranjal</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/experience">Experience</Link>
+        <Link to="/education">Education</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/education" element={<Education />} />
+      </Routes>
     </div>
   )
 }
